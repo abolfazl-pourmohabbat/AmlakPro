429786f5a5d434c736f86dfdc164427f052e2336
Abolfazl Pourmohabbat <abolii2020@gmail.com>
2026-09-17 17:43:49 +0330

    Add customer registration endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,7 +3,13 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken.views import obtain_auth_token
+from core.auth_views import RegisterView
 
-urlpatterns = [path('admin/', admin.site.urls), path('api/', include('core.urls')), path('api/auth/token/', obtain_auth_token)]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
+    path('api/auth/token/', obtain_auth_token),
+    path('api/auth/register/', RegisterView.as_view()),
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
