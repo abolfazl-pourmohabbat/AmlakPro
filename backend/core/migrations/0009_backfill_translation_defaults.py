afc205ffcd6d9cce6ae8a5533dba6cf54621498b
Abolfazl Pourmohabbat <abolii2020@gmail.com>
2026-09-19 03:08:51 +0330

    Make translation backfill database portable

--- backend/core/migrations/0009_backfill_translation_defaults.py
+++ backend/core/migrations/0009_backfill_translation_defaults.py
@@ -1,9 +1,8 @@
 from django.db import migrations
 
 def backfill_translations(apps, schema_editor):
-    with schema_editor.connection.cursor() as cursor:
-        cursor.execute("UPDATE core_officeprofile SET translations = '{}'::jsonb WHERE translations IS NULL")
-        cursor.execute("UPDATE core_property SET translations = '{}'::jsonb WHERE translations IS NULL")
+    apps.get_model('core', 'OfficeProfile').objects.filter(translations__isnull=True).update(translations={})
+    apps.get_model('core', 'Property').objects.filter(translations__isnull=True).update(translations={})
 
 class Migration(migrations.Migration):
     dependencies = [('core','0008_restore_translation_fields')]
